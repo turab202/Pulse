493f8c06b53e839f42c3fb3b677ddbe877c078aa
turab202 <turabturqb@gmail.com>
2026-02-01 18:23:58 +0300

    Chat Detail Screen & Sentry Integration

---
+++ mobile/components/SocketConnection.tsx
@@ -0,0 +1,27 @@
+import { useSocketStore } from "@/lib/socket";
+import { useAuth } from "@clerk/clerk-expo";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+const SocketConnection = () => {
+  const { getToken, isSignedIn } = useAuth();
+  const queryClient = useQueryClient();
+  const connect = useSocketStore((state) => state.connect);
+  const disconnect = useSocketStore((state) => state.disconnect);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      getToken().then((token) => {
+        if (token) connect(token, queryClient);
+      });
+    } else disconnect();
+
+    return () => {
+      disconnect();
+    };
+  }, [isSignedIn, connect, disconnect, getToken, queryClient]);
+
+  return null;
+};
+
+export default SocketConnection;
